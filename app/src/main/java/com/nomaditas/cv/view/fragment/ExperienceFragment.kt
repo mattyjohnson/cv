302c06ebdd65f66c02005800e098d27cd92f6e4d
Matt Johnson <matt@nomaditas.com>
2019-05-28 22:14:29 +0100

    Start of skills and experience fragments.

--- app/src/main/java/com/nomaditas/cv/view/fragment/ExperienceFragment.kt
+++ app/src/main/java/com/nomaditas/cv/view/fragment/ExperienceFragment.kt
@@ -5,10 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.nomaditas.cv.R
+import com.nomaditas.cv.viewmodel.ExperienceViewModel
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ExperienceFragment : Fragment() {
+    private lateinit var model: ExperienceViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        model = ViewModelProviders.of(this).get(ExperienceViewModel::class.java)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_experience, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        model.getExperience().observe(this, Observer {
+            progressBar.visibility = View.GONE
+        })
     }
 }
--- app/src/main/java/com/nomaditas/cv/view/fragment/SkillsFragment.kt
+++ app/src/main/java/com/nomaditas/cv/view/fragment/SkillsFragment.kt
@@ -5,10 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.nomaditas.cv.R
+import com.nomaditas.cv.viewmodel.SkillsViewModel
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 class SkillsFragment : Fragment() {
+    private lateinit var model: SkillsViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        model = ViewModelProviders.of(this).get(SkillsViewModel::class.java)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_skills, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        model.getSkills().observe(this, Observer {
+            progressBar.visibility = View.GONE
+        })
     }
 }
